8827afb656dcd61a70a32a4d22e60482f27313b6
Modrinth1 <k.adeyemi12@hotmail.com>
2025-11-09 12:23:10 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
- 
+namespace logic {
+    /**
+     * Returns the XOR of two boolean values
+     * @param a first value
+     * @param b second value
+     */
+    //% block="(%a) xor (%b)"
+    //% group="Logic"
+    export function xor(a: boolean, b: boolean): boolean {
+        return (a && !b) || (!a && b)
+    }
+}
